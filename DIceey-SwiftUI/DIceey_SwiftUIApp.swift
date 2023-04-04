45fc8fea9d218e3f95751d9df544d83dbb743f73
2thlesss <thomas.jadie.reeves@gmail.com>
2023-04-04 09:01:08 -0400

    Initial Commit

---
+++ DIceey-SwiftUI/DIceey_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIceey_SwiftUIApp.swift
+//  DIceey-SwiftUI
+//
+//  Created by Thomas Jadie Reeves on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DIceey_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
